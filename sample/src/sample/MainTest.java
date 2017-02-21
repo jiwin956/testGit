d5497b9bf143b9ab0155615c9c25b7a67a5dd3bf
JW <JW@JW-PC>
2017-02-21 11:20:04 +0900

    새로 다시

--- sample/src/sample/MainTest.java
+++ sample/src/sample/MainTest.java
@@ -3,11 +3,7 @@ package sample;
 public class MainTest {
 
 	public static void main(String[] args) {
-		System.out.println("깃허브 테스트중");
-		System.out.println("네번째");
-		System.out.println("네번째 되라라라");
-		System.out.println("다섯번째");
-		System.out.println("여섯ㄴㄴ");
+		System.out.println("泥ル쾲吏�");
 	}
 
 }
